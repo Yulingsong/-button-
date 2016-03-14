342a03bfe00c1a91a3c259198e55ddf02335c80c
Yulingsong <yulingsong5@126.com>
2016-03-14 17:18:12 +0800

    Initial Commit

---
+++ 缩放button封装/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  缩放button封装
+//
+//  Created by yulingsong on 16/3/14.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 缩放button封装/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  缩放button封装
+//
+//  Created by yulingsong on 16/3/14.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
